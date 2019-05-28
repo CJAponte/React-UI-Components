155a37b93dabfac4dd6aa2814fb8d706f40ad1f9
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-05-27 20:01:59 -0400

    initial commit and push

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderContainer = props => {
+    return  <div className="headerContainer">
+            Test
+
+            </div>
+}
+
+
+export from 
